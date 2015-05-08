a9fe5027f786f8d7cd5e5eaf2f5c6b2754d3f1b9
Lachlan Musicman <datakid@gmail.com>
2015-05-08 11:46:39 +0000

    added the forms

---
+++ fraser/texts/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+
+from .models import Document
+
+class DocModelForm(forms.ModelForm):
+    body_text = forms.CharField(widget=forms.Textarea)
+
+    class Meta:
+	fields = ['title', 'description', 'date_first', 'date_last', 'collection', 'collection_uma_id', 'uds_number', 'format', 'previous_control_number', 'extent_medium', 'notes', 'irn', 'rights', 'slug', 'body_text', 'text_file', 'image_file', 'pdf_file']        
+	model = Document
