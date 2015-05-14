20da46895295357d567754317fa6b917faa90fcb
Lachlan Musicman <datakid@gmail.com>
2015-05-14 05:35:52 +0000

    moved to the fuller index

--- fraser/texts/urls.py
+++ fraser/texts/urls.py
@@ -3,7 +3,8 @@ from django.conf.urls import url
 from .views import DocumentCorrect, DocumentUpdate, DocumentDetail, DocumentList, electorate_talk_list, press_statement_list, archive_index
 
 urlpatterns = [
-    url(r'^$', DocumentList.as_view(), name='index'),
+    #url(r'^$', DocumentList.as_view(), name='archive_index'),
+    url(r'^$', archive_index, name='archive_index'),
     url(r'^all_archives/$', archive_index, name='archive_index'),
     url(r'^electorates/$', electorate_talk_list, name='electorates'),
     url(r'^press_statements/$', press_statement_list, name='press_statements'),
