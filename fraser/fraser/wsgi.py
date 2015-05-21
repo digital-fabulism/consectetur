7ac7c04b264d45b459c1b34f36e43100d092fe2e
Lachlan Musicman <datakid@gmail.com>
2015-05-21 01:33:47 +0000

    removed wsgi from git; removed secret key from settings

--- fraser/fraser/wsgi.py
+++ fraser/fraser/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fraser.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fraser.settings.dev")
 
 application = get_wsgi_application()
